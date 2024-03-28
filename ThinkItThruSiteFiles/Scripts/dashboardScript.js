0a3f03c7469e06c0838b425ae90630b3b3a81051
lwright249 <158212345+lwright249@users.noreply.github.com>
2024-03-28 12:10:58 -0500

    Almost finished with daily objectives system

--- ThinkItThruSiteFiles/Scripts/dashboardScript.js
+++ ThinkItThruSiteFiles/Scripts/dashboardScript.js
@@ -5,7 +5,7 @@ myTaskList.addTask(new Task("task two", 2, priority.MED, difficulty.HARD, 15, ne
 myTaskList.addTask(new Task("task three", 2, priority.LOW, difficulty.EASY, 15, new Date(2024, 3, 27)));
 //its not adding this one for some reason?
 myTaskList.addTask(new Task("task four", 2, priority.HIGH, difficulty.HARD, 15, new Date(2024, 3, 29)));
-//myTaskList.addTask(new Task("task five", 2, priority.HIGH, difficulty.HARD, 15, new Date(2024, 3, 26)));
+myTaskList.addTask(new Task("task five", 2, priority.HIGH, difficulty.HARD, 15, new Date(2024, 3, 26)));
 myTaskList.logTasks();
 console.log("done");
 
@@ -17,3 +17,17 @@ myTaskList.logTasks();
 let myObj = new DailyObjectivesList(myTaskList);
 myObj.createDayList();
 myObj.printDayList();
+/*
+
+const container = document.getElementById('myDiv');
+const numberList = document.createElement('ul');
+
+//container.innerHTML = '';
+
+for (let i = 1; i <= 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = `Number ${i}`;
+  numberList.appendChild(listItem);
+}
+
+container.appendChild(numberList);*/
--- ThinkItThruSiteFiles/Scripts/objectivesLists.js
+++ ThinkItThruSiteFiles/Scripts/objectivesLists.js
@@ -8,19 +8,21 @@ class DailyObjectivesList{
         //TODO ensure tasks in taskList marked complete when they are checked off on the website
         //also find out if this var is necessary or covered by checkObjComplete??
         this.allObjectivesDone = false;
+
+        this.updateObjectives();
     }
 
-    //slow, O(n^2)
-    //also currently not working, lists numbers in order instead of the right indices!!!!!
+    //slow, O(n^3)??-ish
     createDayList(){
         //this loop might break things
         for(let i = 0; i < this.objTaskList.taskList.length; i++){
             let currTaskDueDate = this.objTaskList.taskList[i].dueDate;
             let hasMatch = false;
+            console.log("task iteration: " + i + " due date: " + currTaskDueDate);
             //check if date of element matches Day that is already made
             this.dayList.forEach(dayElement => {
-                console.log("curr task due date: " + currTaskDueDate);
-                console.log("day Element date: "+ dayElement.dayDate);
+                //console.log("curr task due date: " + currTaskDueDate);
+                //console.log("day Element date: "+ dayElement.dayDate);
                 if(currTaskDueDate.getTime() == dayElement.dayDate.getTime()){
                     console.log("equal");
                     hasMatch = true;
@@ -36,13 +38,77 @@ class DailyObjectivesList{
             if(hasMatch == false){
                 let newDay = new Day(currTaskDueDate);
                 newDay.add(i);
-                this.dayList.push(newDay);
+                //TODO change to push in place!!!!
+                //this.dayList.push(newDay);
+                this.pushInPlace(newDay);
             }
         }
     }
 
+    pushInPlace(dayToPush){
+        let isPushedInMiddle = false;
+
+        console.log("daylist len: " + this.dayList.length);
+
+        for(let i = 0; i < this.dayList.length; i++){
+            if (dayToPush.dayDate.getTime() < this.dayList[i].dayDate.getTime() && !isPushedInMiddle){
+                isPushedInMiddle = true;
+                console.log("pusing to index " + i);
+                this.dayList.splice(i, 0, dayToPush);
+            }
+        }
+        if(isPushedInMiddle == false){
+            this.dayList.push(dayToPush);
+        }
+    }
+
     updateObjectives(){
-        
+        let oldCeiling = 0;
+        let newCeiling = 0;
+        let totalTime = 0;
+        let timeToAdd = 0;
+        let timeToWorkInDay = 0;
+        let today = new Day();
+
+        this.dayList.forEach(day => {
+            //totals time to work in this day
+            day.indexList.forEach(index => {
+                timeToWorkInDay += this.objTaskList.taskList[index].getTimeRemaining();
+            });
+
+            totalTime += timeToWorkInDay;
+
+            //check if this day's date is less than today
+            if((day.dayDate.getDate() - today.getDate()) >= 1){
+                newCeiling = totalTime / ((day.dayDate.getDate() - today.getDate())+1);
+            }
+            //if task overdue, it treats it as if the task is due today (just like being divided by one)
+            else{
+                newCeiling = totalTime;
+            }
+
+            //if more work per day needs to be completed to finish all tasks, add on more work!
+            if(newCeiling > oldCeiling){
+
+                day.indexList.forEach(index => {
+                    let timeToWorkOnTask = this.objTaskList.taskList[index].getTimeRemaining();
+                    timeToAdd = (newCeiling - oldCeiling) * (timeToWorkOnTask / timeToWorkInDay);
+                    //TODO add task to objectivesList
+                    //myTaskList.addTask(new Task("task five", 2, priority.HIGH, difficulty.HARD, 15, new Date(2024, 3, 26)));
+
+                    //using push instead of addTask() for speed :3
+                    
+                });
+            }
+
+            oldCeiling = newCeiling;
+
+        });
+
+
+
+
+
     }
 
     checkObjectivesComplete(){
@@ -72,6 +138,7 @@ class Day{
     constructor(dayDate){
         this.dayDate = dayDate;
         this.indexList = [];
+        
     }
 
     add(taskIndex){
