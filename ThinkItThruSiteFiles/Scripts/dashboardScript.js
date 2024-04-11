88b76fac8a67680dbdbf0ecbd49030f38a62af1b
lwright249 <158212345+lwright249@users.noreply.github.com>
2024-04-11 13:45:31 -0500

    Fixed queue score finally! Also implemented some html generation

--- ThinkItThruSiteFiles/Scripts/dashboardScript.js
+++ ThinkItThruSiteFiles/Scripts/dashboardScript.js
@@ -1,11 +1,11 @@
 console.log("Hello World!");
 let myTaskList = new TaskList();
-myTaskList.addTask(new Task("task one", 200, priority.HIGH, 15, new Date(2024, 3, 5)));
-myTaskList.addTask(new Task("task two", 200, priority.MED, 15, new Date(2024, 3, 6)));
-myTaskList.addTask(new Task("task three", 200, priority.LOW, 15, new Date(2024, 3, 4)));
+myTaskList.addTask(new Task("task one", 200, priority.HIGH, new Date(2024, 3, 5)));
+myTaskList.addTask(new Task("task two", 200, priority.MED, new Date(2024, 3, 6)));
+myTaskList.addTask(new Task("task three", 200, priority.LOW, new Date(2024, 3, 4)));
 //its not adding this one for some reason?
-myTaskList.addTask(new Task("task four", 200, priority.HIGH, 15, new Date(2024, 3, 6)));
-myTaskList.addTask(new Task("task five", 200, priority.HIGH, 15, new Date(2024, 3, 3)));
+myTaskList.addTask(new Task("task four", 200, priority.HIGH, new Date(2024, 3, 6)));
+myTaskList.addTask(new Task("task five", 200, priority.HIGH, new Date(2024, 3, 3)));
 myTaskList.logTasks();
 console.log("done");
 
@@ -13,6 +13,7 @@ let taskToChange = myTaskList.getTask(1);
 taskToChange.name = "CHANGED";
 
 myTaskList.logTasks();
+generateTasks();
 
 let myObj = new DailyObjectivesList(myTaskList);
 myObj.printDayList();
@@ -71,11 +72,21 @@ node.innerHTML('<p>some dynamic html</p>');*/
 
   var dashboardTasks = document.getElementById("TaskBoxSection");
   let html = "";
+  let numTasksDisplayed = 0;
 
-  myTaskList.forEach(Task => {
+  console.log(dashboardTasks);
 
+  myTaskList.taskList.forEach(Task => {
+    numTasksDisplayed++;
+    console.log("ADDING TO DASHBOARD: " + Task.name);
     html += "<div class=\"TaskBox\"> <div class=\"Task\"><div class=\"EditTaskBtn\"><a href=\"javascript:void(0);\" class=\"icon\" onclick=\"showLinks()\"><img src=\"Styles/more-horiz.svg\"></img> </a></div><div class=\"TaskName\"><h3>Task 1</h3> <p>Time Left: 10:04:31</p></div><div class=\"PauseAndPlay\"><h3>Pause/Play</h3> <p>Time Spent: 1:02:15</p></div><div class=\"CheckBtn\"><button><span>&#9744</span><span id=\"checkmark\">&#10008</span></button></div>\</div>\<div class=\"Subtask\"><p>subtask1</p></div><div class=\"Subtask\">subtask2</div></div>"
   } );
 
-  dashboardTasks.innerHTML(html);
+  //console.log(html);
+
+  //dashboardTasks.innerHTML = ('<p> hi </p>');
+  //console.log(dashboardTasks.innerHTML);
+  dashboardTasks.innerHTML = html;
+  console.log("NUM TASKS DISPLAYED = " + numTasksDisplayed);
+  
 }
