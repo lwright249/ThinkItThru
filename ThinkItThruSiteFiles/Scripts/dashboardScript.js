3c5d1d07881b9a766cdeeb9da5797d1a903f494a
lwright249 <158212345+lwright249@users.noreply.github.com>
2024-04-11 14:05:58 -0500

    functionality to display tasks

--- ThinkItThruSiteFiles/Scripts/dashboardScript.js
+++ ThinkItThruSiteFiles/Scripts/dashboardScript.js
@@ -72,21 +72,64 @@ node.innerHTML('<p>some dynamic html</p>');*/
 
   var dashboardTasks = document.getElementById("TaskBoxSection");
   let html = "";
-  let numTasksDisplayed = 0;
 
   console.log(dashboardTasks);
 
-  myTaskList.taskList.forEach(Task => {
-    numTasksDisplayed++;
+  /*myTaskList.taskList.forEach(Task => {
     console.log("ADDING TO DASHBOARD: " + Task.name);
     html += "<div class=\"TaskBox\"> <div class=\"Task\"><div class=\"EditTaskBtn\"><a href=\"javascript:void(0);\" class=\"icon\" onclick=\"showLinks()\"><img src=\"Styles/more-horiz.svg\"></img> </a></div><div class=\"TaskName\"><h3>Task 1</h3> <p>Time Left: 10:04:31</p></div><div class=\"PauseAndPlay\"><h3>Pause/Play</h3> <p>Time Spent: 1:02:15</p></div><div class=\"CheckBtn\"><button><span>&#9744</span><span id=\"checkmark\">&#10008</span></button></div>\</div>\<div class=\"Subtask\"><p>subtask1</p></div><div class=\"Subtask\">subtask2</div></div>"
   } );
+*/
+  for(let i = 0; i < myTaskList.taskList.length; i++){
+    html += htmlForTask(myTaskList.taskList[i], i);
+  }
 
   //console.log(html);
 
   //dashboardTasks.innerHTML = ('<p> hi </p>');
   //console.log(dashboardTasks.innerHTML);
-  dashboardTasks.innerHTML = html;
-  console.log("NUM TASKS DISPLAYED = " + numTasksDisplayed);
+  dashboardTasks.innerHTML = html;  
+}
+
+/*
+
+
+        <div class="TaskBox">
+            <div class="Task">
+                <div class="EditTaskBtn"><a href="javascript:void(0);" class="icon" onclick="showLinks()"><img src="Styles/more-horiz.svg"></img> </a></div>
+                <div class="TaskName"><h3>Task 1</h3> <p>Time Left: 10:04:31</p></div>
+                <div class="PauseAndPlay"><h3>Pause/Play</h3> <p>Time Spent: 1:02:15</p></div>
+                <div class="CheckBtn"><button><span>&#9744</span><span id="checkmark">&#10008</span></button></div>
+            </div>
+
+            <div class="Subtask"><p>subtask1</p></div>
+            <div class="Subtask">subtask2</div>
+        </div>
+
+        */
+
+function htmlForTask(Task, index){
+  let html = "<div id = \"TaskBox" + index + "\">";
+  html += "<div class=\"Task " + index + "\">";
+  html += "<div class=\"EditTaskBtn\"><a href=\"javascript:void(0);\" class=\"icon\" onclick=\"showLinks()\"><img src=\"Styles/more-horiz.svg\"></img> </a></div>";
+  html += "<div class= TaskName><h3>" + Task.name + "</h3> <p>Time Left: " + Task.getTimeRemaining() + "</p></div>"; //TODO format date
+  html += "<div class=\"PauseAndPlay\"><h3>Pause/Play</h3> <p>Time Spent:"+ Task.timeWorked +"</p></div>";
+  html += "<div class=\"CheckBtn\"><button><span>&#9744</span><span id=\"checkmark\">&#10008</span></button></div></div>"; //TODO connect this to Task somehow
   
+  if(Task.hasSubTasks == true){
+    if(Task.subtasksList == null){
+      console.log("Levi made a coding error");
+    }
+    else{
+      for(let i = 0; i < Task.subtasksList.length; i++){
+        html += "<div class=\"Subtask\"><p>"+ Task.subtasksList[i].name +"</p></div>";
+      }
+    }
+  }
+
+
+  html += "</div>";
+
+  return html;
+
 }
