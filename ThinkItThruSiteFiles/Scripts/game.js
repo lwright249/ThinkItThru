490dc8ecf960c49649a333f2378a9aee69e7b707
ztrav <travistech2000@hotmail.com>
2024-04-22 23:00:31 -0500

    Added Nav Bar to Game

---
+++ ThinkItThruSiteFiles/Scripts/game.js
@@ -0,0 +1,8 @@
+function showLinks() {
+    var x = document.getElementById("LinkContainer");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
