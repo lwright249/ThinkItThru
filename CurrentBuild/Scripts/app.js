23285f9f22e90d620ba26d2112f1914ffdb0aa51
JensenSchmidt <jensenschmidt18@gmail.com>
2024-04-24 07:51:48 -0500

    More Modifications (Authentication is Driving Me Crazy I don't know what's wrong)

--- CurrentBuild/Scripts/app.js
+++ CurrentBuild/Scripts/app.js
@@ -8,6 +8,11 @@ const auth = getAuth();
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const signUpBtn = document.getElementById("signup-btn");
+const UIErrorMessage = document.getElementById("error-message");
+
+const signUpFormView = document.getElementById("signup-form");
+const userProfileView = document.getElementById("user-profile");
+const UIuserEmail = document.getElementById("user-email");
 
 const signUpButtonPressed = async (e) => {
     e.preventDefault();
@@ -18,10 +23,29 @@ const signUpButtonPressed = async (e) => {
             email.value, 
             password.value
         );
-        console.log(userCredential)    
+        console.log(userCredential);
+
+        UIuserEmail.innerHTML = userCredential.user.email;
+        
+        signUpFormView.style.display = "none";
+        userProfileView.style.display = "block";
     } catch (error) {
         console.log(error.code);
+        UIErrorMessage.innerHTML = formatErrorMessage(error.code);
+        UIErrorMessage.classList.add("visible");
     }
 };
 
 signUpBtn.addEventListener("click", signUpButtonPressed);
+
+const formatErrorMessage = (errorCode) => {
+    let message = "";
+    if(errorCode === "auth/invalid-email" || errorCode === "auth/missing-email") {
+        message = "Please enter a valid email."
+    } else if (errorCode === "auth/missing-password" || errorCode === "auth/weak-password") {
+        message = "Password must be at least 6 characters.";
+    } else if (errorCode === "auth/email-already-in-use") {
+        message = "Email is already taken.";
+    }
+    return message;
+};
